89ffa17d65ba08bfef4ea8bd4b4ee6c691c6f494
Paulino Auyon <pauyon@jgboswell.com>
2024-03-07 15:11:57 -0800

    Ignore header/column order for spreadsheets

--- PageChecker.Library/XmlReaderUtility.cs
+++ PageChecker.Library/XmlReaderUtility.cs
@@ -10,6 +10,9 @@ public static class XmlReaderUtility
     public static XLWorkbook MarketWorkbook { get; internal set; } = new XLWorkbook();
     public static XLWorkbook SalesWorkbook { get; internal set; } = new XLWorkbook();
 
+    public static List<string> MarketHeaders { get; internal set; } = new();
+    public static List<string> SalesHeaders { get; internal set; } = new();
+
 
     public static IXLWorksheet GetMarketWorksheet(int index)
     {
@@ -37,6 +40,8 @@ public static class XmlReaderUtility
 
         var worksheet = GetMarketWorksheet(1);
 
+        MarketHeaders = GetWorksheetHeaders(worksheet, skip: 1, take: 1);
+
         var rows = worksheet.RangeUsed().RowsUsed().Skip(2); // Skip header row
 
         foreach (var row in rows)
@@ -46,15 +51,15 @@ public static class XmlReaderUtility
                 break;
             }
 
-            var customer = row.Cell(1).Value.ToString();
-            var size = row.Cell(2).Value.ToString();
-            var rep = row.Cell(3).Value.ToString();
-            var categories = row.Cell(4).Value.ToString();
-            var contractStatus = row.Cell(5).Value.ToString();
-            var artwork = row.Cell(6).Value.ToString();
-            var notes = row.Cell(7).Value.ToString();
-            var placement = row.Cell(8).Value.ToString();
-            var accountingNotes = row.Cell(9).Value.ToString();
+            var customer = row.Cell(MarketHeaders.IndexOf("Customer") + 1).Value.ToString();
+            var size = row.Cell(MarketHeaders.IndexOf("Size") + 1).Value.ToString();
+            var rep = row.Cell(MarketHeaders.IndexOf("Rep") + 1).Value.ToString();
+            var categories = row.Cell(MarketHeaders.IndexOf("Categories") + 1).Value.ToString();
+            var contractStatus = row.Cell(MarketHeaders.IndexOf("Contract Status") + 1).Value.ToString();
+            var artwork = row.Cell(MarketHeaders.IndexOf("Artwork") + 1).Value.ToString();
+            var notes = row.Cell(MarketHeaders.IndexOf("Notes") + 1).Value.ToString();
+            var placement = row.Cell(MarketHeaders.IndexOf("Placement") + 1).Value.ToString();
+            //var accountingNotes = row.Cell(MarketHeaders.IndexOf("ACCOUNTING USE ONLY") + 1).Value.ToString();
 
 
             marketData.Add(new Market
@@ -67,7 +72,7 @@ public static class XmlReaderUtility
                 Artwork = artwork,
                 Notes = notes,
                 Placement = placement,
-                AccountingNotes = accountingNotes,
+                //AccountingNotes = accountingNotes,
             });
         }
 
@@ -79,17 +84,19 @@ public static class XmlReaderUtility
         var salesData = new List<SalesRun>();
 
         var worksheet = GetSalesWorksheet(1);
+        
+        SalesHeaders = GetWorksheetHeaders(worksheet, skip: 0, take: 1);
 
         var rows = worksheet.RangeUsed().RowsUsed().Skip(1); // Skip header row
 
         foreach (var row in rows)
         {
-            var client = row.Cell(1).Value.ToString();
-            var product = row.Cell(2).Value.ToString();
-            var description = row.Cell(3).Value.ToString();
-            var salesRep = row.Cell(4).Value.ToString();
-            var net = row.Cell(5).Value.ToString();
-            var barter = row.Cell(6).Value.ToString();
+            var client = row.Cell(SalesHeaders.IndexOf("Client") + 1).Value.ToString();
+            var product = row.Cell(SalesHeaders.IndexOf("Product") + 1).Value.ToString();
+            var description = row.Cell(SalesHeaders.IndexOf("Description") + 1).Value.ToString();
+            var salesRep = row.Cell(SalesHeaders.IndexOf("Sales Rep") + 1).Value.ToString();
+            var net = row.Cell(SalesHeaders.IndexOf("Net") + 1).Value.ToString();
+            var barter = row.Cell(SalesHeaders.IndexOf("Barter") + 1).Value.ToString();
 
 
             salesData.Add(new SalesRun
@@ -166,20 +173,16 @@ public static class XmlReaderUtility
         var workbook = new XLWorkbook();
         var ws = workbook.Worksheets.Add("Report");
 
-        ws.Range(1, 1, 1, 10).Style.Fill.SetBackgroundColor(XLColor.LightBlue);
+        ws.Range(1, 1, 1, MarketHeaders.Take(6).Count()).Style.Fill.SetBackgroundColor(XLColor.LightBlue);
 
         var rowNum = 1;
+        var colNum = 1;
 
-        ws.Cell(rowNum, 1).Value = "PassedCheck";
-        ws.Cell(rowNum, 2).Value = "Customer";
-        ws.Cell(rowNum, 3).Value = "Size";
-        ws.Cell(rowNum, 4).Value = "Rep";
-        ws.Cell(rowNum, 5).Value = "Categories";
-        ws.Cell(rowNum, 6).Value = "Contract Status";
-        /*ws.Cell(rowNum, 7).Value = "Artwork";
-        ws.Cell(rowNum, 8).Value = "Notes";
-        ws.Cell(rowNum, 9).Value = "Placement";
-        ws.Cell(rowNum, 10).Value = "Accounting Notes";*/
+        foreach(var header in MarketHeaders.Take(6))
+        {
+            ws.Cell(rowNum, colNum).Value = header;
+            colNum++;
+        }
 
         foreach (var item in checkedMarketData)
         {
@@ -191,27 +194,41 @@ public static class XmlReaderUtility
             ws.Cell(rowNum, 4).Value = item.Rep;
             ws.Cell(rowNum, 5).Value = item.Categories;
             ws.Cell(rowNum, 6).Value = item.ContractStatus;
-            /*ws.Cell(rowNum, 7).Value = item.Artwork;
-            ws.Cell(rowNum, 8).Value = item.Notes;
-            ws.Cell(rowNum, 9).Value = item.Placement;
-            ws.Cell(rowNum, 10).Value = item.AccountingNotes;*/
 
             ws.Range(rowNum, 1, 1, 1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
 
             if (!item.PassedCheck) 
             {
-                ws.Range(rowNum, 1, rowNum, 10).Style.Fill.SetBackgroundColor(XLColor.Yellow);
+                ws.Range(rowNum, 1, rowNum, MarketHeaders.Take(6).Count()).Style.Fill.SetBackgroundColor(XLColor.Yellow);
             }
 
             if (item.ContractStatus.ToLower() == "pay per lead")
             {
-                ws.Range(rowNum, 1, rowNum, 10).Style.Fill.SetBackgroundColor(XLColor.LightPastelPurple);
+                ws.Range(rowNum, 1, rowNum, MarketHeaders.Take(6).Count()).Style.Fill.SetBackgroundColor(XLColor.LightPastelPurple);
             }
         }
 
         ws.Columns().AdjustToContents();
 
         workbook.SaveAs(resultsFilePath);
+    }
+
+    public static List<string> GetWorksheetHeaders(IXLWorksheet worksheet, int skip, int take)
+    {
+        var headers = new List<string>();
+        var headerRow = worksheet.RangeUsed().RowsUsed().Skip(skip).Take(take);
+
+        foreach (var row in headerRow)
+        {
+            var cells = row.Cells();
+
+            foreach (var cell in cells)
+            {
+                headers.Add(cell.Value.ToString());
+            }
+        }
+
+        return headers;
     }
 
     public static void SetRootDirectoryPath(string directoryPath)
